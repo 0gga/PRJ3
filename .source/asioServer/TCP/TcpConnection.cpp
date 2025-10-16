2072f6532b724337b422f686347edc70672f6b17
Oscar Friis Gustavsen <oscar@friisweb.dk>
2025-10-16 11:47:06 +0200

    refactor

--- .source/asioServer/TCP/TcpConnection.cpp
+++
@@ -1,9 +0,0 @@
-#include "TcpConnection.h"
-#include <iostream>
-
-TcpConnection::TcpConnection(boost::asio::ip::tcp::socket socket) : socket(std::move(socket)) {}
-
-TcpConnection::~TcpConnection() {
-	boost::system::error_code ec;
-	socket.close(ec);
-}
--- .source/asioServer/src/main.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "ReaderHandler.h"
-
-int main() {
-	ReaderHandler reader(9000, 9001);
-
-	ReaderHandler::runLoop();
-
-	return 0;
-}
