10d96e93f0b1b08e95217e81ea1549d53ed6cd1f
Ogga <oscar@friisweb.dk>
2025-11-14 11:21:03 +0100

    Readme fixes

--- source/client/pn532-lib/rpi_tcptest/rpi_get_uid.cpp
+++ source/client/pn532-lib/rpi_tcptest/rpi_get_uid.cpp
@@ -1,18 +1,13 @@
 #include "rpi_client.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
+    int portno            = (argc > 1) ? atoi(argv[1]) : 9000;
+    const char* server_ip = (argc > 2) ? argv[2] : "172.16.15.3";
 
-    int portno = (argc > 1) ? atoi(argv[1]) : 9000;
-    const char *server_ip = (argc > 2) ? argv[2] : "172.16.15.3";
-
-    try
-    {
+    try {
         client rpi_client(portno, server_ip);
         rpi_client.run();
-    }
-    catch (std::exception &e)
-    {
+    } catch (std::exception& e) {
         std::cout << "Error:" << e.what() << std::endl;
         return -1;
     }
