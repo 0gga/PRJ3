33cf10b97e6021ac0420c969730c4e72c15c8789
Freja Nielsen <202405893@post.au.dk>
2025-11-28 10:57:07 +0100

    Finished csv-logger

--- source/asioServer/logger/csv.hpp
+++ source/asioServer/logger/csv.hpp
@@ -2,49 +2,21 @@
 
 class CsvLogger {
     public:
-        // constructor
-
-        // addLog adds a new line in the csv-file for the corresponding date
-        // it adds data for these specs: Date, Time, Door, Name, UserID, Acces
-        // void addLog(const std::string& info);
+        /// addLog adds a new line in the csv-file for the corresponding date
+        /// it adds data for these specs: Date, Time, Door, Name, UserID, Acces
+        /// void addLog(const std::string& info);
         void addLog(std::string door, std::string name, int userID, std::string acces);
 
-        // getLogByName transfers the csv-file with the corresponding date
-        // to the admin pc using TCP
+        /// getLogByName transfers the csv-file with the corresponding date
+        /// to the admin pc using TCP
         std::string getLogByDate(std::string date);
 
-        // getLogByName transfers the csv-file with the corresponding name
+        /// getLogByName transfers the csv-file with the corresponding name
         std::string getLogByName(std::string name);
 
-        // getLogByDoor transfers the csv-file withe the corresponding door name
+        /// getLogByDoor transfers the csv-file withe the corresponding door name
         std::string getLogByDoor(std::string door);
-
-        
-    private:
-        
 
 };
 
-
-/*
-class CsvLogger {
-public:
-	// constructor
-	void log(const std::string& info);
-	std::string getLog() const;
-
-	// addLog adds a new line in the csv-file for the corresponding date
-	// it adds data for these specs: Date, Time, Door, Name, UserID, Access
-	// void addLog(const std::string& info);
-	void addLog(std::string door, std::string name, int userID, std::string access);
-
-	// getLogByName transfers the csv-file with the corresponding date
-	// to the admin pc using TCP
-	std::string getLogByDate(std::string date);
-
-	// getLogByName transfers the csv-file with the corresponding name
-	std::string getLogByName(std::string);
-
-private:
-}; */
 
