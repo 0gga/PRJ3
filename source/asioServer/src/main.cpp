558066e38a6e0c474c74a44893b25029da130d17
0gga <oscar@friisweb.dk>
2025-11-29 00:01:40 +0100

    fixes to CMake for args

--- source/asioServer/src/main.cpp
+++ source/asioServer/src/main.cpp
@@ -3,27 +3,17 @@
 #include <ogga/scopetimer.hpp>
 #include <ogga/csvlog.hpp>
 
-#ifdef Args
+/// CMake options: -DARGS=1 to use args
 int main(int argc, char* argv[]) {
-    ReaderHandler reader(argv[1], argv[2], std::to_string(argv[3])); // Use arguments for client- & cliPort parameters
-
-    ogga::scopetimer runTimer("Server ran for", "min");
-
-    ogga::print("Server Successfully Constructed");
-
-    ReaderHandler::runLoop();
-
-    return 0;
-}
-
+#ifdef ARGS
+	[[maybe_unused]] ReaderHandler reader(*argv[1], *argv[2], argv[3]); // Use arguments for client- & cliPort parameters
 #else
-int main() {
-    [[maybe_unused]] ReaderHandler reader(9000, 9001, "adminReader");
-
-    [[maybe_unused]] ogga::scopetimer runTimer("Server Uptime", "ns");
-    ogga::print("Server Successfully Constructed");
-
-    ReaderHandler::runLoop();
-    return 0;
-}
+	[[maybe_unused]] ReaderHandler reader(9000, 9001, "adminReader");
 #endif
+
+	[[maybe_unused]] ogga::scopetimer runTimer("Server Uptime", "ns");
+	ogga::print("Server Successfully Constructed");
+
+	ReaderHandler::runLoop();
+	return 0;
+}
